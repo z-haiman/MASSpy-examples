48ff081fd7f96b96b0dee60562746c698204010a
z-haiman <zhaiman@eng.ucsd.edu>
2019-10-07 19:44:39 -0700

    Updated using embeded html exported

--- examples/SB2/scripts/execute-notebooks.sh
+++ examples/SB2/scripts/execute-notebooks.sh
@@ -2,8 +2,13 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 sb2_dir="$parent_path/../"
+html_dir="$parent_path/../html/"
 
 cd $sb2_dir
 
 jupyter nbconvert --to notebook --execute SB2_*.ipynb  --inplace
 jupyter nbconvert --to notebook --execute SB2-Chapter-*.ipynb --inplace --ExecutePreprocessor.timeout=-1
+
+jupyter nbconvert --to html_embed SB2-Chapter-*.ipynb\
+ --TagRemovePreprocessor.remove_input_tags='{"remove_cell"}'\
+ --output-dir $html_dir
